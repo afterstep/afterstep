8986f487f0542699f6635ea256d1a84c7ad65e94
sashav <sashav>
2000-07-28 03:11:11 +0000

    ashash_add_create_destroy_init

--- libAfterBase/ashash.c
+++ libAfterBase/ashash.c
@@ -35,6 +35,147 @@
 #include "../include/afterstep.h"
 #include "../include/ashash.h"
 
+ASHashKey default_hash_func(ASHashableValue value, ASHashKey hash_size)
+{
+    return value.long_val%hash_size;
+}
+
+Bool default_compare_func(ASHashableValue value1, ASHashableValue value2)
+{
+    return (value1.long_val==value2.long_val);
+}
+
+void 
+init_ashash( ASHashTable *hash, Bool freeresources )
+{
+    if( hash )
+    {
+	if( freeresources )
+	    if( hash->buckets ) free( hash->buckets );
+	memset( hash, 0x00, sizeof( ASHashTable ));
+    }
+}
+
+ASHashTable*
+create_ashash( ASHashKey size, 
+               ASHashKey (*hash_func)(ASHashableValue, ASHashKey), 
+	       Bool      (*compare_func)(ASHashableValue, ASHashableValue),
+	       void (*item_destroy_func)(ASHashItem *))
+{
+  ASHashTable *hash;
+    
+    if( size <= 0 ) return NULL ;
+    
+    hash = safemalloc( sizeof(ASHashTable) );  
+    init_ashash( hash, False );
+    
+    hash->buckets = safemalloc( sizeof(ASHashBucket)*size );
+    memset( hash->buckets, 0x00, sizeof(ASHashBucket)*size );
+    
+    hash->size = size ;
+    
+    if( hash_func )     hash->hash_func = hash_func ;
+    else		hash->hash_func = default_hash_func ;
+
+    if( compare_func )  hash->compare_func = compare_func ;    
+    else		hash->compare_func = default_compare_func ;    
+    
+    hash->item_destroy_func = item_destroy_func ;
+    
+    return hash ;
+}
+
+static void
+destroy_ashash_bucket( ASHashBucket *bucket, void (*item_destroy_func)(ASHashItem *))
+{
+  register ASHashItem 	*item, *next ;
+    for( item = *bucket ; item != NULL ; item = next )
+    {
+	next = item->next ;
+	if( item_destroy_func )
+	    item_destroy_func( item );
+	else
+	    free( item );
+    } 
+    *bucket = NULL ;
+}
+
+void 
+destroy_ashash( ASHashTable **hash )
+{
+    if( *hash )
+    {
+      register int i ;
+        for( i = (*hash)->size-1 ; i >=0 ; i-- )
+	    if( (*hash)->buckets[i] )
+		destroy_ashash_bucket( &((*hash)->buckets[i]), (*hash)->item_destroy_func );
+		
+	init_ashash( *hash, True );    
+	free( *hash );
+	*hash = NULL ;
+    }
+}
+
+static ASHashResult 
+add_item_to_bucket( ASHashBucket *bucket, 
+                    ASHashItem *item, 
+		    Bool (*compare_func)(ASHashableValue, ASHashableValue) )
+{
+  ASHashItem *tmp ;
+    /* first check if we already have this item */
+    for( tmp = *bucket ; tmp != NULL ; tmp = tmp->next )
+	if( compare_func( tmp->value, item->value ) )
+	    return (tmp->data == item->data )?ASH_ItemExistsSame:ASH_ItemExistsDiffer ;
+    /* now actually add this item */
+    item->next = (*bucket) ;
+    *bucket = item ;
+    return ASH_Success ;
+}
+
+ASHashResult add_hash_item( ASHashTable *hash, ASHashableValue value, void *data )
+{
+  ASHashKey key ;
+  ASHashItem *item ;
+  
+    if( hash == NULL ) return False ;
+
+    key = hash->hash_func( value, hash->size );	
+    if( key >= hash->size ) return False ;
+    
+    item = safemalloc( sizeof(ASHashItem) );	
+    item->next = NULL ;
+    item->value = value ;
+    item->data = data ;
+    
+    return add_item_to_bucket( &(hash->buckets[key]), item, hash->compare_func );
+}
+
+void*
+get_hash_item( ASHashTable *hash, ASHashableValue value )
+{
+  ASHashKey key ;
+  ASHashItem *item = NULL;
+  
+    if( hash ) 
+    {
+	key = hash->hash_func( value, hash->size );	
+	if( key < hash->size )
+	{
+	    
+	/* TODO */
+	
+	}
+    }
+    return item ;
+}
+
+
+
+
+/************************************************************************/
+/************************************************************************/
+/* 	Some usefull implementations 					*/
+/************************************************************************/
 ASHashKey option_hash_value (ASHashableValue value, ASHashKey hash_size)
 {
   ASHashKey hash_key = 0;
--- libAfterBase/ashash.h
+++ libAfterBase/ashash.h
@@ -16,21 +16,37 @@ typedef struct ASHashItem
 }ASHashItem ;
 
 typedef unsigned int ASHashKey ;
+typedef ASHashItem* ASHashBucket ;
 
 typedef struct ASHashTable
 {
-    ASHashKey size ;
-    ASHashItem *items ;
-    ASHashKey (*hash_func)(ASHashableValue value, ASHashKey hash_size);
+    ASHashKey 	size ;
+    ASHashBucket *buckets ;
+    ASHashKey 	(*hash_func)(ASHashableValue value, ASHashKey hash_size);
+    Bool      	(*compare_func)(ASHashableValue value1, ASHashableValue value2);
+    void 	(*item_destroy_func)(ASHashItem *item);
 }ASHashTable;
 
 void init_ashash( ASHashTable *hash, Bool freeresources );
-ASHashTable *create_ashash( ASHashKey size, ASHashKey (*hash_func)(ASHashableValue value, ASHashKey hash_size));
-void destroy_ashash( ASHashTable *hash );
+ASHashTable *create_ashash( ASHashKey size, 
+                            ASHashKey (*hash_func)(ASHashableValue, ASHashKey), 
+			    Bool      (*compare_func)(ASHashableValue, ASHashableValue),
+			    void (*item_destroy_func)(ASHashItem *));
+void destroy_ashash( ASHashTable **hash );
 
-Bool add_hash_item( ASHashTable *hash, ASHashableValue value, void *data );
-Bool remove_hash_item( ASHashTable *hash, ASHashableValue value );
+typedef enum {
+
+    ASH_BadParameter 	= -3,
+    ASH_ItemNotExists   = -2,
+    ASH_ItemExistsDiffer= -1,
+    ASH_ItemExistsSame 	=  0,
+    ASH_Success		=  1
+}ASHashResult ;
+
+ASHashResult add_hash_item( ASHashTable *hash, ASHashableValue value, void *data );
 void *get_hash_item( ASHashTable *hash, ASHashableValue value );
+ASHashResult remove_hash_item( ASHashTable *hash, ASHashableValue value );
+ASHashResult destroy_hash_item( ASHashTable *hash, ASHashableValue value );
 
 /* here is the set of implemented hash functions : */
 /* configuration options - case unsensitive and spaces are not alowed */
