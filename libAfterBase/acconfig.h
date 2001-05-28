09795e957ab5212a0f658fe7425cca8781a7a9ac
sashav <sashav>
2001-05-28 06:36:20 +0000

    Created prototype for the new library structure - libAfterBase. There will be two more libraries : libAfterImage and libAfterStep. This is to split generic and non-generic code and allow for libraries to be used separately  - as a preparation for libAfterImage release as a stand-alone library.)

---
+++ libAfterBase/acconfig.h
@@ -0,0 +1,16 @@
+/* defined when inline, __inline__ or __inline keywords are not available    */
+#undef NO_INLINE
+
+#undef I18N
+
+/* debugging options */
+#undef  DEBUG_ALLOCS
+#undef  DEBUG_TRACE
+#undef  DEBUG_TRACE_X
+
+/* self diagnostic signal handler's argument */
+#undef HAVE_SIGCONTEXT
+
+/* self diagnostic SIGSEGV handling (as opposed to dumping core) */
+#undef HAVE_SIGSEGV_HANDLING
+
