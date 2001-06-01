efe183783fce0e0d1bf1686dec17500ead4fca05
sashav <sashav>
2001-06-01 06:56:33 +0000

    started up libAfterImage

---
+++ libAfterImage/acconfig.h
@@ -0,0 +1,17 @@
+/* defined when inline, __inline__ or __inline keywords are not available    */
+#undef NO_INLINE
+
+#undef I18N
+
+/* debugging options */
+#undef  DEBUG_ALLOCS
+
+#undef  HAVE_XPM
+#undef  HAVE_XPM_X11
+
+#undef  HAVE_JPEG
+#undef  HAVE_PNG
+#undef  HAVE_GIF
+#undef  HAVE_TIFF
+#undef  HAVE_FREETYPE
+
