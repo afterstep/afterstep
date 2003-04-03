1012c9ac8f39322e1afda1c62ee9356e8bc087b5
sasha <sasha>
2003-04-03 21:52:17 +0000

    added xpm2png conversion script

---
+++ libAfterImage/apps/xpm2png.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+if test "x$1" = "x" ; then
+  echo "Usage : xpm2png.sh <source_image> [<destination_image>] [alpha_blur_radius]";
+else
+  if test "x$2" = "x" ; then
+    out_fname="$1_out";
+    echo "Using output filename $out_fname"
+  else
+    out_fname=$2;
+  fi
+  if test "x$3" = "x" ; then
+    radius=3;
+    echo "Using default blur radius $radius"
+  else
+    radius=$3;
+  fi
+  ascompose -n -o $out_fname -t png -s "<blur horz=$radius vert=$radius channels=\"a\"><img src=$1/> </blur>";
+fi
