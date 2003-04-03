be7614ab400657f38f97528b42a75d6ce6b8b4dd
sasha <sasha>
2003-04-03 23:32:39 +0000

    compiled set of logos

---
+++ afterstep/desktop/icons/logos/png/scale48.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+if test "x$1" = "x" ; then
+  echo "Usage : scale48.sh <source_image> [<destination_image>]";
+else
+  if test "x$2" = "x" ; then
+    out_fname="$1_out";
+    echo "Using output filename $out_fname"
+  else
+    out_fname=$2;
+  fi
+  ascompose -n -o $out_fname -t png -s "<scale width=48 height=48><img src=$1/> </scale>";
+fi
