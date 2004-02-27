c3679fc52dda85931afa5bc7e68d965c19cf2be8
sasha <sasha>
2004-02-27 22:52:53 +0000

    Implemented new Banner using ascompose

--- libAfterBase/fs.c
+++ libAfterBase/fs.c
@@ -18,7 +18,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-#define LOCAL_DEBUG
+#undef LOCAL_DEBUG
 
 #include "config.h"
 
