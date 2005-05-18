b7538cfdd52720e5fe4995133dfeb8aaa165bf5f
sasha <sasha>
2005-05-18 22:40:59 +0000

    Implemented background list in ASWallpaper as a GtkTreeView

--- src/tools/ASWallpaper/main.c
+++ src/tools/ASWallpaper/main.c
@@ -5,8 +5,11 @@
 
 #include "../../../../configure.h"
 #include "../../../../libAfterStep/asapp.h"
+#include "../../../../libAfterStep/module.h"
+#include "../../../../libAfterConf/afterconf.h"
 
 
+#include <gdk/gdkx.h>
 #include <gtk/gtk.h>
 
 #include "interface.h"
@@ -15,17 +18,33 @@
 int
 main (int argc, char *argv[])
 {
-  GtkWidget *window1;
-  
+	GdkDisplay *gdk_display ;
+  	GtkWidget *main_window;
+	int i ; 
+	static char *deleted_arg = "_deleted_arg_" ;
+
+  	InitMyApp (CLASS_ASCP, argc, argv, NULL, NULL, 0 );
+	for( i = 1 ; i < argc ; ++i ) 
+		if( argv[i] == NULL ) 
+			argv[i] = deleted_arg ;
+  	LinkAfterStepConfig();
+  	InitSession();
 
 #ifdef ENABLE_NLS
-  bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
-  bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-  textdomain (GETTEXT_PACKAGE);
+  	bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+  	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+  	textdomain (GETTEXT_PACKAGE);
 #endif
 
-  gtk_set_locale ();
-  gtk_init (&argc, &argv);
+  	gtk_set_locale ();
+  	gtk_init (&argc, &argv);
+
+	gdk_display = gdk_display_get_default();
+	
+	ConnectXDisplay (gdk_x11_display_get_xdisplay(gdk_display), NULL, False);
+	ReloadASEnvironment( NULL, NULL, NULL, False, True );
+	
+	/*MyArgs.display_name*/
 
   add_pixmap_directory (DEFAULT_PIXMAP_DIR);
 
@@ -34,8 +53,8 @@ main (int argc, char *argv[])
    * (except popup menus), just so that you see something after building
    * the project. Delete any components that you don't want shown initially.
    */
-  window1 = create_window1 ();
-  gtk_widget_show (window1);
+  main_window = create_main_window ();
+  gtk_widget_show (main_window);
   
 
   gtk_main ();
