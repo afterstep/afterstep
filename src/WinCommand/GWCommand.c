a4fa5e9d2c433e6f7505ab167d05d12acfa05640
fabs <fabs>
2005-06-17 00:40:49 +0000

    GWCommand is now almost completely functional.

--- src/WinCommand/GWCommand.c
+++ src/WinCommand/GWCommand.c
@@ -19,6 +19,8 @@
 #include "../../configure.h"
 #include "../../libAfterStep/asapp.h"
 #include "../../libAfterStep/module.h"
+#include "../../libAfterStep/ascommand.h"
+#include "../../libAfterStep/operations.h"
 #include "../../libAfterConf/afterconf.h"
 
 
@@ -31,165 +33,387 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define COMMAND "WinCommand"
+/* ugly gui-definitions */
+  GtkWidget *window1;
+  GtkWidget *frame1;
+  GtkWidget *alignment1;
+  GtkWidget *table1;
+  GtkWidget *alldesks_radio;
+  GSList *alldesks_radio_group = NULL;
+  GtkWidget *thisdesk_radio;
+  GtkWidget *thisscreen_radio;
+  GtkWidget *frame2;
+  GtkWidget *alignment2;
+  GtkWidget *pattern_entry;
+  GtkWidget *label2;
+  GtkWidget *action_combo;
+  GtkWidget *frame3;
+  GtkWidget *alignment3;
+  GtkWidget *notebook1;
+  GtkWidget *label13;
+  GtkWidget *table2;
+  GtkWidget *label9;
+  GtkWidget *label10;
+  GtkWidget *entry2;
+  GtkWidget *entry1;
+  GtkWidget *hbox1;
+  GtkWidget *label11;
+  GtkWidget *entry3;
+  GtkWidget *label3;
+  GtkWidget *label1;
+  GtkEntryCompletion *completion;
+  GtkTreeModel *completion_model;
+/* /ugly gui-definitions */
 
-struct gui
+
+typedef struct
 {
-	GtkWidget *dialog;
-	GtkWidget *dialog_vbox;
-	GtkWidget *vbox;
-	GtkWidget *entry;
-	GtkWidget *label;
-	GtkWidget *dialog_action_area;
-}gw_gui;
+	char *name;
+	int param_page;
+	void (*exec_wrapper)(void);
+} action_t ;
 
 
-/* Prototypes */
-char *join_with_whitespace(int nelems, char **ar);
-void on_entry_activate ( GtkEntry *entry, gpointer data );
-void init_gui(char *params, char **argv);
-void usage(void);
+struct ASGWCommandState
+{
+	char *action;
+	ASFlagType flags;
+	char **win_list;
+} GWCommandState;
 
-char *params;
+/******** Prototypes *********************/
+GtkWidget* create_window (void);
+static void destroy( GtkWidget *widget,
+                     gpointer   data );
+void no_args_wrapper(void);
+void move_wrapper(void);
+void send_to_desk_wrapper(void);
+void jump_wrapper(void);
 
-/* Returns a pointer to a string consisting 
- * of nelems elements from ar seperated by whitespace
- */
+/******** /Prototypes ********************/
 
-char *
-join_with_whitespace(int nelems, char **ar)
+action_t Actions[] = 
+{
+	{"iconify", 0, no_args_wrapper },
+	{"kill", 0, no_args_wrapper },
+	{"move", 1, move_wrapper},
+	{"sendtodesk", 2, send_to_desk_wrapper},
+	{"jump", 0, jump_wrapper},
+	{ NULL, 0, NULL }
+};
+
+/* Creates a tree model containing the completions */
+GtkTreeModel *
+create_simple_completion_model (void)
+{
+	GtkListStore *store;
+	GtkTreeIter iter;
+	int i;
+	
+	store = gtk_list_store_new (1, G_TYPE_STRING);
+	
+	for( i = 0 ;
+	     GWCommandState.win_list[i] != NULL; i++ )
+	{
+		gtk_list_store_append (store, &iter);
+	  gtk_list_store_set (store, &iter, 0, GWCommandState.win_list[i], -1);
+	}
+	
+	return GTK_TREE_MODEL (store);
+}
+
+GtkWidget*
+create_window (void)
+{
+  
+
+	window1 = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title (GTK_WINDOW (window1), "GWCommand");
+	
+	frame1 = gtk_frame_new (NULL);
+	gtk_widget_show (frame1);
+	gtk_container_add (GTK_CONTAINER (window1), frame1);
+	gtk_container_set_border_width (GTK_CONTAINER (frame1), 5);
+	gtk_frame_set_shadow_type (GTK_FRAME (frame1), GTK_SHADOW_NONE);
+
+	alignment1 = gtk_alignment_new (0.5, 0.5, 1, 1);
+	gtk_widget_show (alignment1);
+	gtk_container_add (GTK_CONTAINER (frame1), alignment1);
+	gtk_alignment_set_padding (GTK_ALIGNMENT (alignment1), 0, 5, 5, 5);
+	
+	table1 = gtk_table_new (3, 3, FALSE);
+	gtk_widget_show (table1);
+	gtk_container_add (GTK_CONTAINER (alignment1), table1);
+	
+	alldesks_radio = gtk_radio_button_new_with_mnemonic (NULL, "on all Desktops");
+	gtk_widget_show (alldesks_radio);
+	gtk_table_attach (GTK_TABLE (table1), alldesks_radio, 2, 3, 2, 3,
+			  (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
+			  (GtkAttachOptions) (0), 0, 0);
+	gtk_radio_button_set_group (GTK_RADIO_BUTTON (alldesks_radio), alldesks_radio_group);
+	alldesks_radio_group = gtk_radio_button_get_group (GTK_RADIO_BUTTON (alldesks_radio));
+	
+	thisdesk_radio = gtk_radio_button_new_with_mnemonic (NULL, "on this Desktop");
+	gtk_widget_show (thisdesk_radio);
+	gtk_table_attach (GTK_TABLE (table1), thisdesk_radio, 1, 2, 2, 3,
+			  (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
+			  (GtkAttachOptions) (0), 0, 0);
+	gtk_radio_button_set_group (GTK_RADIO_BUTTON (thisdesk_radio), alldesks_radio_group);
+	alldesks_radio_group = gtk_radio_button_get_group (GTK_RADIO_BUTTON (thisdesk_radio));
+	
+	thisscreen_radio = gtk_radio_button_new_with_mnemonic (NULL, "on this screen");
+	gtk_widget_show (thisscreen_radio);
+	gtk_table_attach (GTK_TABLE (table1), thisscreen_radio, 0, 1, 2, 3,
+			  (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
+			  (GtkAttachOptions) (0), 0, 0);
+	gtk_radio_button_set_group (GTK_RADIO_BUTTON (thisscreen_radio), alldesks_radio_group);
+	alldesks_radio_group = gtk_radio_button_get_group (GTK_RADIO_BUTTON (thisscreen_radio));
+	
+	frame2 = gtk_frame_new (NULL);
+	gtk_widget_show (frame2);
+	gtk_table_attach (GTK_TABLE (table1), frame2, 0, 3, 1, 2,
+			  (GtkAttachOptions) (GTK_FILL),
+			  (GtkAttachOptions) (GTK_EXPAND | GTK_FILL), 5, 5);
+	gtk_container_set_border_width (GTK_CONTAINER (frame2), 5);
+	gtk_frame_set_shadow_type (GTK_FRAME (frame2), GTK_SHADOW_NONE);
+	
+	alignment2 = gtk_alignment_new (0.5, 0.5, 1, 1);
+	gtk_widget_show (alignment2);
+	gtk_container_add (GTK_CONTAINER (frame2), alignment2);
+	
+	pattern_entry = gtk_entry_new ();
+	gtk_widget_show (pattern_entry);
+	gtk_container_add (GTK_CONTAINER (alignment2), pattern_entry);
+	
+	completion = gtk_entry_completion_new();
+	gtk_entry_set_completion (GTK_ENTRY (pattern_entry), completion);
+	g_object_unref (completion);
+	
+	/* Create a tree model and use it as the completion model */
+	completion_model = create_simple_completion_model ();
+	gtk_entry_completion_set_model (completion, completion_model);
+	g_object_unref (completion_model);
+	
+	/* Use model column 0 as the text column */
+	gtk_entry_completion_set_text_column (completion, 0);
+	
+	label2 = gtk_label_new ("<b>pattern</b>");
+	gtk_widget_show (label2);
+	gtk_frame_set_label_widget (GTK_FRAME (frame2), label2);
+	gtk_label_set_use_markup (GTK_LABEL (label2), TRUE);
+	gtk_misc_set_padding (GTK_MISC (label2), 5, 0);
+	
+	action_combo = gtk_combo_box_new_text ();
+	gtk_widget_show (action_combo);
+	gtk_table_attach (GTK_TABLE (table1), action_combo, 0, 1, 0, 1,
+			  (GtkAttachOptions) (GTK_FILL),
+			  (GtkAttachOptions) (0), 0, 0);
+	
+	
+	frame3 = gtk_frame_new (NULL);
+	gtk_widget_show (frame3);
+	gtk_table_attach (GTK_TABLE (table1), frame3, 1, 3, 0, 1,
+			  (GtkAttachOptions) (GTK_FILL),
+			  (GtkAttachOptions) (GTK_FILL), 5, 0);
+	gtk_container_set_border_width (GTK_CONTAINER (frame3), 5);
+	gtk_frame_set_shadow_type (GTK_FRAME (frame3), GTK_SHADOW_NONE);
+	
+	alignment3 = gtk_alignment_new (0.5, 0.5, 1, 1);
+	gtk_widget_show (alignment3);
+	gtk_container_add (GTK_CONTAINER (frame3), alignment3);
+	
+	notebook1 = gtk_notebook_new ();
+	gtk_widget_show (notebook1);
+	gtk_container_add (GTK_CONTAINER (alignment3), notebook1);
+	GTK_WIDGET_UNSET_FLAGS (notebook1, GTK_CAN_FOCUS);
+	gtk_notebook_set_show_tabs (GTK_NOTEBOOK (notebook1), FALSE);
+	gtk_notebook_set_show_border (GTK_NOTEBOOK (notebook1), FALSE);
+	
+	label13 = gtk_label_new ("No arguments");
+	gtk_widget_show (label13);
+	gtk_container_add (GTK_CONTAINER (notebook1), label13);
+	
+	
+	table2 = gtk_table_new (2, 2, TRUE);
+	gtk_widget_show (table2);
+	gtk_container_add (GTK_CONTAINER (notebook1), table2);
+	
+	label9 = gtk_label_new ("X:");
+	gtk_widget_show (label9);
+	gtk_table_attach (GTK_TABLE (table2), label9, 0, 1, 0, 1,
+			  (GtkAttachOptions) (GTK_FILL),
+			  (GtkAttachOptions) (0), 0, 0);
+	gtk_misc_set_alignment (GTK_MISC (label9), 0, 0.5);
+	
+	label10 = gtk_label_new ("Y:");
+	gtk_widget_show (label10);
+	gtk_table_attach (GTK_TABLE (table2), label10, 0, 1, 1, 2,
+			  (GtkAttachOptions) (GTK_FILL),
+			  (GtkAttachOptions) (0), 0, 0);
+	gtk_misc_set_alignment (GTK_MISC (label10), 0, 0.5);
+	
+	entry2 = gtk_entry_new ();
+	gtk_widget_show (entry2);
+	gtk_table_attach (GTK_TABLE (table2), entry2, 1, 2, 1, 2,
+			  (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
+			  (GtkAttachOptions) (0), 0, 0);
+	
+	entry1 = gtk_entry_new ();
+	gtk_widget_show (entry1);
+	gtk_table_attach (GTK_TABLE (table2), entry1, 1, 2, 0, 1,
+			  (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
+			  (GtkAttachOptions) (0), 0, 0);
+	
+	hbox1 = gtk_hbox_new (FALSE, 0);
+	gtk_widget_show (hbox1);
+	gtk_container_add (GTK_CONTAINER (notebook1), hbox1);
+	
+	label11 = gtk_label_new ("Desk: ");
+	gtk_widget_show (label11);
+	gtk_box_pack_start (GTK_BOX (hbox1), label11, FALSE, FALSE, 0);
+	
+	entry3 = gtk_entry_new ();
+	gtk_widget_show (entry3);
+	gtk_box_pack_start (GTK_BOX (hbox1), entry3, TRUE, TRUE, 0);
+	
+	label3 = gtk_label_new ("<b>arguments</b>");
+	gtk_widget_show (label3);
+	gtk_frame_set_label_widget (GTK_FRAME (frame3), label3);
+	gtk_label_set_use_markup (GTK_LABEL (label3), TRUE);
+	gtk_misc_set_padding (GTK_MISC (label3), 5, 0);
+	
+	label1 = gtk_label_new ("<b>Command to perform on a window:</b>");
+	gtk_widget_show (label1);
+	gtk_frame_set_label_widget (GTK_FRAME (frame1), label1);
+	gtk_label_set_use_markup (GTK_LABEL (label1), TRUE);
+	gtk_misc_set_padding (GTK_MISC (label1), 5, 0);
+	
+	gtk_widget_grab_focus (pattern_entry);
+	return window1;
+}
+
+action_t *get_action_by_name(const char *needle)
 {
 	int i;
-	int size = 0;
-	char *res;
-	
-	/* Calculate size */
-	for ( i = 0; i < nelems; ++i)
-	{
-		if(ar[i] == NULL)
-			continue;
-		/* We add an additional char for whitespace/'\0'*/
-		size += strlen(ar[i]) + 1;
-	}
+	for( i = 0; Actions[i].name != NULL; i++)
+		if(mystrcasecmp(Actions[i].name, needle) == 0)
+			return &Actions[i];
+	return NULL;
+}
 
-	/* Allocate memory for buffer */
-	res = (char *) malloc( sizeof(char) * size);
-	if(res == NULL)
-		return NULL;
-	
-	/* Join strings. */
-	for( i = 0; i < nelems; ++i)
-	{
-		if(ar[i] == NULL)
-			continue;
-		strcat(res, ar[i]);
-		if(i+1 != nelems)
-			strcat(res , " ");
-	}
+int get_index_by_action_name(const char *needle)
+{
+	int i;
+	for( i = 0; Actions[i].name != NULL; i++)
+		if(mystrcasecmp(Actions[i].name, needle) == 0)
+			return i;
+	return -1;
+}
 
-	return res;
-	
+/* Callback-functions */
+static void destroy( GtkWidget *widget,
+                     gpointer   data )
+{
+    gtk_main_quit ();
 }
 
 void
-on_entry_activate ( GtkEntry *entry,
+on_action_combo_changed                (GtkComboBox     *combobox,
+                                        gpointer         user_data)
+{
+	action_t *a;
+	GWCommandState.action =
+		gtk_combo_box_get_active_text (GTK_COMBO_BOX( action_combo ));
+	
+	a = get_action_by_name(GWCommandState.action);
+
+	gtk_notebook_set_current_page (GTK_NOTEBOOK(notebook1), a->param_page );
+}
+
+void
+on_pattern_activate ( GtkEntry *entry,
 		    gpointer data )
 {
-	FILE *res;
-	char res_buf[1000];
-	char com[1000];
+	action_t *act;
+	clear_selection();
+	
+	/* call the wrapper */
+	act = get_action_by_name(GWCommandState.action);
+	act->exec_wrapper();
 
-	int chars_read;
-
-	const gchar * pattern = gtk_entry_get_text( entry );
-
-	
-	/* Launch the program */
-	snprintf(com, 1000 - 1 , "%s %s -pattern %s", COMMAND, params, pattern);
-	
-	fprintf( stderr, com);
-	if ( ( res = popen ( com, "r")) == NULL)
-		return;
-	
-	while ( (chars_read = fread(res_buf, 1, 1000 - 1, res)) > 0 )
-	{
-		res_buf[chars_read - 1] = '\0';
-		fprintf(stdout, res_buf);
-	}
-	
-	
 	gtk_main_quit();
 }
 
-void
-init_gui(char *params, char **argv)
+/*********************************/
+
+void fill_action_combo( void )
 {
-	struct gui *g = &gw_gui;
-	g->dialog = gtk_dialog_new();
-	
-	gtk_window_set_title (GTK_WINDOW (g->dialog), "GWCommand");
-	gtk_window_set_modal (GTK_WINDOW (g->dialog), TRUE);
-	gtk_window_set_skip_taskbar_hint (GTK_WINDOW (g->dialog), TRUE);
-	gtk_window_set_type_hint (GTK_WINDOW (g->dialog), GDK_WINDOW_TYPE_HINT_DIALOG);
-
-	g->dialog_vbox = GTK_DIALOG (g->dialog)->vbox;
-	gtk_widget_show (g->dialog_vbox);
-
-	g->vbox = gtk_vbox_new (FALSE, 0);
-	gtk_widget_show (g->vbox);
-	gtk_box_pack_start (GTK_BOX (g->dialog_vbox), g->vbox, FALSE, FALSE, 0);
-
-	g->label = gtk_label_new (params);
-	gtk_widget_show (g->label);
-	gtk_box_pack_start (GTK_BOX (g->vbox), g->label, FALSE, FALSE, 0);
-
-	g->entry = gtk_entry_new ();
-	gtk_widget_show (g->entry);
-	gtk_box_pack_start (GTK_BOX (g->vbox), g->entry, FALSE, FALSE, 0);
-
-	g->dialog_action_area = GTK_DIALOG (g->dialog)->action_area;
-	gtk_widget_show (g->dialog_action_area);
-	gtk_button_box_set_layout (GTK_BUTTON_BOX (g->dialog_action_area), GTK_BUTTONBOX_END);
-
-	
-	/* Callbacks */
-	g_signal_connect ((gpointer) g->entry, "activate",
-			  G_CALLBACK (on_entry_activate),
-			  argv);
-	
-	gtk_widget_show(g->dialog);
-
+	int i;
+	for( i = 0; Actions[i].name != NULL; i++)
+		gtk_combo_box_append_text
+			(GTK_COMBO_BOX (action_combo), Actions[i].name );
 }
 
+/* exec_wrappers */
+
 void
-usage(void)
+no_args_wrapper(void)
 {
-	g_print("Supply at least one argument\n");
+	select_windows_by_pattern
+		(gtk_entry_get_text(GTK_ENTRY(pattern_entry)), False, False);
+	ascom_do(GWCommandState.action, NULL);
 }
 
+void move_wrapper(void)
+{
+	
+}
+
+void send_to_desk_wrapper(void)
+{
+	send_to_desk_params p;
+	p.desk = atoi (gtk_entry_get_text(GTK_ENTRY(entry3)));
+	select_windows_by_pattern
+		(gtk_entry_get_text(GTK_ENTRY(pattern_entry)), False, False);
+
+	ascom_do(GWCommandState.action, &p);
+}
+
+void jump_wrapper(void)
+{
+	select_windows_by_pattern
+		(gtk_entry_get_text(GTK_ENTRY(pattern_entry)), True, False);
+	ascom_do(GWCommandState.action, NULL);
+}
+
+/****************************/
 
 int main(int argc, char **argv)
 {
 	GdkDisplay *gdk_display ;
-	int i ; 
+	GtkWidget *my_window;
+	int i, index;
 	static char *deleted_arg = "_deleted_arg_" ;
 
+	memset( &GWCommandState, 0x00, sizeof(GWCommandState));
+
   	InitMyApp (CLASS_ASCP, argc, argv, NULL, NULL, 0 );
-	for( i = 1 ; i < argc ; ++i ) 
+	for( i = 1 ; i < argc ; ++i ){ 
 		if( argv[i] == NULL ) 
-			argv[i] = strdup(deleted_arg) ;
-  	LinkAfterStepConfig();
-  	InitSession();
-	
-	
-	if( argc < 2)
-	{
-		usage();
-		return -1;
+			argv[i] = deleted_arg ;
+		/* It's a flag */
+		else if(argv[i][0] == '-')
+		{
+			
+		}else
+		{
+			/* It's the command! */
+			GWCommandState.action = argv[i];
+		}
 	}
-	
-	params = join_with_whitespace(argc - 1, argv + 1);
-	
+
+	LinkAfterStepConfig();
+  	InitSession();
 	
 	gtk_init( &argc, &argv);
 	gdk_display = gdk_display_get_default();
@@ -197,15 +421,60 @@ int main(int argc, char **argv)
 	LoadColorScheme();
 	ConnectXDisplay (gdk_x11_display_get_xdisplay(gdk_display), NULL, False);
 	ReloadASEnvironment( NULL, NULL, NULL, False, True );
-    
+ 
+   
 	ConnectAfterStep (WINDOW_CONFIG_MASK |
                       WINDOW_NAME_MASK |
                       M_END_WINDOWLIST|
 				      M_NEW_DESKVIEWPORT, 0);
 	
-	init_gui( params, argv);
+	ascom_init();
+	ascom_update_winlist();
+
+	GWCommandState.win_list = ascom_get_win_names();
+	
+	my_window = create_window();
+	
+	/* Fill interface with data */
+	fill_action_combo();
+	
+	if(GWCommandState.action == NULL)
+		index = 0;
+	else
+		index = get_index_by_action_name(GWCommandState.action);
+	
+	if( index == -1 )
+		index = 0;
+	
+	GWCommandState.action = Actions[index].name;
+
+	gtk_combo_box_set_active(GTK_COMBO_BOX(action_combo), index);
+	gtk_notebook_set_current_page (GTK_NOTEBOOK(notebook1), Actions[index].param_page);
+
+	/* connect signals */
+	g_signal_connect (G_OBJECT (my_window), "destroy",
+			  G_CALLBACK (destroy), NULL);
+	
+	g_signal_connect ((gpointer) action_combo, "changed",
+			  G_CALLBACK (on_action_combo_changed),
+			  NULL);
+
+	g_signal_connect ((gpointer) pattern_entry, "activate",
+			 G_CALLBACK (on_pattern_activate), NULL);
+
+
+	gtk_widget_show(my_window);
 	gtk_main();
 	
-	free(params);
+	/* Deinit */
+	ascom_wait();
+	ascom_deinit();
+	
+	/* free memory */
+	i = 0;
+	while( GWCommandState.win_list[i] != NULL)
+		free( GWCommandState.win_list[i++] );
+	free( GWCommandState.win_list );
+	
 	return 0;
 }
