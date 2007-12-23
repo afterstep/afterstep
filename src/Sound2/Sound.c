a41ec515437aa4ddf31f965f740ee278ca807e45
jeremy <jeremy>
2007-12-23 06:26:56 +0000

    Rewrite of Sound2 Module.

--- src/Sound2/Sound.c
+++ src/Sound2/Sound.c
@@ -19,19 +19,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h> 
 #include <alsa/asoundlib.h>
-// This is for the debugging.. and prolly will just remove it
+// This is for the debugging.. and prolly will just remove it      
 #include <time.h>
 
 // AS Includes
 #include "../../configure.h"
 #include "../../libAfterStep/asapp.h"
-#include "../../libAfterStep/screen.h"
+#include "../../libAfterStep/screen.h"  
 #include "../../libAfterStep/event.h"
 #include "../../libAfterStep/wmprops.h"
 #include "../../libAfterConf/afterconf.h"
 #include "../../libAfterStep/clientprops.h"
+#include "Sound.h"
 // Defines
 #define MAX_SOUNDS AFTERSTEP_EVENTS_NUM
 #define mask_reg MAX_MASK
@@ -44,183 +45,159 @@ void DeadPipe (int);
 void GetBaseOptions (const char *filename/* unused */);
 void GetOptions (const char *filename);
 void HandleEvents();
-void DispatchEvent (ASEvent * event);
+void DispatchEvent (ASEvent * event);  
 void proc_message (send_data_type type, send_data_type *body);
 int ply_sound(int code);
 
+// Sound2 Functions
+int as_snd2_init_stg1();
+int as_snd2_init_stg2();
+int as_snd2_playsound();
+
 Bool (*audio_play) (short) = NULL;
 
-// audio.c
-//char         *sound_table[MAX_SOUNDS];
-
-int main (int argc,char ** argv)
+int main(int argc,char ** argv)
 {
-
-        set_DeadPipe_handler(DeadPipe);
-        InitMyApp (CLASS_SOUND, argc, argv, NULL, NULL, 0 );
-        LinkAfterStepConfig();
-
-        ConnectX( ASDefaultScr, PropertyChangeMask );
-        ConnectAfterStep ( mask_reg, 0 );
-
-        // declared via AudioConfig *Config.... (after defines..)
-        Config = CreateAudioConfig();
-
-	int err;
-	snd_pcm_t *playback_handle;
-	snd_pcm_hw_params_t *hw_params;
-	int DEBUG1;
-	int ret1;
-	int SRate, dir;
-	typedef char *String;
-	//--
-	FILE* WaveFile;
-	int frames;
-	SRate = 22000;
-	DEBUG1 = *argv[1];
-	
-	fprintf(stdout,"ARGX: %i::%s :: %s\n",argc,argv[1],argv[2]);
-	
-	dir = 0;
-	String pcmDeviceID = "plughw:0,0";
-
-	if ((err = snd_pcm_open (&playback_handle, pcmDeviceID, SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
-		fprintf (stderr, "cannot open audio device %s (%s)\n", 
-			 argv[1],
-			 snd_strerror (err));
-		exit (1);
-	}
-	
-	if (DEBUG1) { fprintf(stdout,"Opened PCM Device\n"); }
-	   
-	if ((err = snd_pcm_hw_params_malloc (&hw_params)) < 0) {
-		fprintf (stderr, "cannot allocate hardware parameter structure (%s)\n",
-			 snd_strerror (err));
-		exit (1);
-	}
-			 
-	if (DEBUG1) { fprintf(stdout,"Allocated Params: %i\n",err); }
-
-	if ((err = snd_pcm_hw_params_any (playback_handle,hw_params)) < 0) {
-		fprintf (stderr, "cannot initialize hardware parameter structure (%s)\n",
-			 snd_strerror (err));
-		exit (1);
-	}
-	
-	if (DEBUG1) { fprintf(stdout,"Init Hardware Params\n"); }
-
-	if ((err = snd_pcm_hw_params_set_access (playback_handle,hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
-		fprintf (stderr, "cannot set access type (%s)\n",
-			 snd_strerror (err));
-		exit (1);
-	}
-	
-	if (DEBUG1) { fprintf(stdout,"Set access Type\n"); }
-
-	if ((err = snd_pcm_hw_params_set_format (playback_handle,hw_params, SND_PCM_FORMAT_S16_LE)) < 0) {
-		fprintf (stderr, "cannot set sample format (%s)\n",
-			 snd_strerror (err));
-		exit (1);
-	}
-	
-	if (DEBUG1) { fprintf(stdout,"Set Sample Format\n"); }
-
-	// Last two args for this must be pointers. else it will cause a segfault.
-	// ?: SRate wont work unless its pointer (in test app). when linked to AS,
-	//  : it cant be a pointer.
-	// target/chosen exact value is <,=,> val following dir (-1,0,1) :: hmm
-	ret1 = snd_pcm_hw_params_set_rate_near(playback_handle,hw_params,SRate,&dir);
-	
-	if (DEBUG1) { fprintf(stdout,"Set Sample Rate: %i :: %i\n", ret1, dir); }
-
-	if ((err = snd_pcm_hw_params_set_channels (playback_handle,hw_params, 2)) < 0) {
-		fprintf (stderr, "cannot set channel count (%s)\n",snd_strerror(err));
-		exit (1);
-	}
-	
-	if (DEBUG1) { fprintf(stdout,"Channel Count\n"); }
-
-	if ((err = snd_pcm_hw_params (playback_handle,hw_params)) < 0) {
-		fprintf (stderr, "cannot set parameters (%s)\n",
-			 snd_strerror (err));
-		exit (1);
-	}
-
-	if (DEBUG1) { fprintf(stdout,"Params Set\n"); }
-
-	snd_pcm_hw_params_free (hw_params);
-	
-	if (DEBUG1) { fprintf(stdout,"Params Free\n"); }
-
-	if ((err = snd_pcm_prepare (playback_handle)) < 0) {
-		fprintf (stderr, "cannot prepare audio interface for use (%s)\n",
-			 snd_strerror (err));
-		exit (1);
-	}
-	
-	if (DEBUG1) { fprintf(stdout,"Prepare to Play!\n"); }
-	
-// --------------------------------------
-	int size, read, fcount;
-	long r;
-	char *buffer;
-
-	WaveFile = fopen("online.wav","rb");
-	if (WaveFile == NULL) { fprintf(stdout,"File Open Failed\n"); }
-	fprintf(stdout,"Wave: %p\n",WaveFile);
-	
-	fseek(WaveFile,0,SEEK_END);
-	size = ftell(WaveFile);
-	rewind(WaveFile);
-	fseek(WaveFile,128,SEEK_SET);
-	
-	frames = snd_pcm_bytes_to_frames(playback_handle,size);
-	
-	fprintf(stdout,"Size: %d FRAMES: %d\n",size,frames);
-
-	fcount = 0;
-
-	buffer = (char *)malloc(size);
-	while ((read = fread(buffer,4,frames,WaveFile)) > 0)
-	{
-	        fprintf(stdout,"WHILE %d\n",fcount);
-		r = snd_pcm_writei(playback_handle,buffer,frames);
-		fcount++;
-	}
-
-// --------------------------------------
-	snd_pcm_close (playback_handle);
-	
-	HandleEvents();
-	
-//	exit (0);
-
-        return 0;
+    // Standard AS Stuff
+    set_DeadPipe_handler(DeadPipe);
+    InitMyApp(CLASS_SOUND,argc,argv,NULL,NULL,0);
+    LinkAfterStepConfig();
+    
+    ConnectX(ASDefaultScr,PropertyChangeMask);
+    ConnectAfterStep(mask_reg,0);
+    
+    // need to figure out what this is for...
+    Config = CreateAudioConfig();
+    
+    as_snd2_init_stg1();
+    //as_snd2_init_stg2();
+    
+    //HandleEvents();
+    
+    return 0;
 }
-// ------ READ HEADER ------
-/*
-int asSound_rfileh(char *FileName)
-{
-    // Find out what kind of file we got... 
-    FILE *WaveFile;
-    char *buffer1;
-    int *Head1;
 
-    WaveFile = fopen("online.wav","rb");
-    // Alloc mem for fread.. 
-    buffer1 = (char*)malloc(4096);
-    // Read one element of 4 bytes.
-    // (the first part of wave header)
-    Head1 = fread(buffer1,4,1,WaveFile);
-    if (Head1 == "RIFF")
+int as_snd2_init_stg1()
+{
+    // Stage 1 Sound2 module Init.
+    struct SND2dev SND2devS, *SND2devP;
+    SND2devP = &SND2devS;
+    
+    typedef char *String;
+    int Snd2DEBUG, stg2Ret, ret1;
+    String pcmDeviceID = "plughw:0,0"; // Need to make Config File opt for setting this.
+
+    Snd2DEBUG = 1;
+
+    if (Snd2DEBUG) { fprintf(stdout,"Opening PCM Device....\n"); }
+    
+    // (Returned PCM Handle, ASCII Ident of Handle, Wanted Stream, Open Mode)
+    snd_pcm_open(&SND2devS.pcm_handle,pcmDeviceID,SND_PCM_STREAM_PLAYBACK,0);
+  
+    if (Snd2DEBUG) { fprintf(stdout,"Allocating Hardware Param Structure....\n"); }
+    
+    // (Returned Pointer)
+    ret1 = snd_pcm_hw_params_malloc(&SND2devP->hw_params);
+    
+    if (Snd2DEBUG) { fprintf(stdout,"Init Hardware Param Structure...\n");}
+    
+    // (PCM Handle, Config Space)
+    snd_pcm_hw_params_any(SND2devS.pcm_handle,SND2devS.hw_params);
+    
+    if (Snd2DEBUG) { fprintf(stdout,"Setting Access Type...\n"); }
+    
+    // (PCM Handle, Config Space, Access Type)
+    snd_pcm_hw_params_set_access(SND2devS.pcm_handle,SND2devS.hw_params,SND_PCM_ACCESS_RW_INTERLEAVED);
+    
+    if (Snd2DEBUG) { fprintf(stdout,"Setting Sample Format...\n"); }
+    
+    // (PCM Handle, Config Space, Format)
+    // SND_PCM_FORMAT_S16_LE = Signed 16-bit Little Endian
+    // May need to var-ize last part, and detect cpu type on configure.
+    snd_pcm_hw_params_set_format(SND2devS.pcm_handle,SND2devS.hw_params,SND_PCM_FORMAT_S16_LE);
+    
+    if (Snd2DEBUG) { fprintf(stdout,"Stage 1 Init Complete!\n\n"); }
+    
+    stg2Ret = as_snd2_init_stg2(SND2devP);
+    
+    return 1;
+}
+
+int as_snd2_init_stg2(struct SND2dev * SND2devP)
+{
+    // Following Init depends on the file type.
+    // since we're setting Sample rate, channel count (mono,stereo), etc.
+    
+    int Snd2DEBUG;
+    int SRate, CHcount, dir;
+    
+    SRate = 22000;
+    CHcount = 2;
+
+    Snd2DEBUG = 1;
+
+    if (Snd2DEBUG) { fprintf(stdout,"Setting Sample Rate...\n"); }
+    
+    // (PCM Handle, Config Space, Sample Rate, Sub Unit Direction)
+    snd_pcm_hw_params_set_rate_near(SND2devP->pcm_handle,SND2devP->hw_params,SRate,&dir);
+    
+    if (Snd2DEBUG) { fprintf(stdout,"Setting Channel Count...\n"); }
+    
+    // (PCM Handle, Config Space, Channel Count)
+    snd_pcm_hw_params_set_channels(SND2devP->pcm_handle,SND2devP->hw_params,CHcount);
+    
+    if (Snd2DEBUG) { fprintf(stdout,"Apply Hardware Params...\n"); }
+    
+    // (PCM Handle, Config Space)
+    snd_pcm_hw_params(SND2devP->pcm_handle,SND2devP->hw_params);
+    
+    if (Snd2DEBUG) { fprintf(stdout,"Free Hardware Space Container...\n"); }
+    
+    // (Config Space)
+    snd_pcm_hw_params_free(SND2devP->hw_params);
+    
+    if (Snd2DEBUG) { fprintf(stdout,"Prepare To Play!...\n"); }
+    
+    // (PCM Handle)
+    snd_pcm_prepare(SND2devP->pcm_handle);
+    
+    if (Snd2DEBUG) { fprintf(stdout,"Stage 2 Init Complete!\n\n"); }
+    
+    HandleEvents(SND2devP);
+    
+    return 1;
+}
+
+int as_snd2_playsound(struct SND2dev * SND2devP)
+{
+    // Lets play some sound now!
+    
+    char *SFbuffer;
+    int SNDFileSize, SNDFileFrames, SNDFileRead;
+    FILE *SNDFile;
+    
+    SNDFile = fopen("online.wav","rb");
+    if (SNDFile == NULL) { fprintf(stdout,"Failed to open Sound File\n"); exit(1); }
+    
+    
+    fseek(SNDFile,0,SEEK_END); // We're trying to get the full file size here...
+    SNDFileSize = ftell(SNDFile);
+    rewind(SNDFile); // Now that we got size, go back to beginning
+    fseek(SNDFile,128,SEEK_SET); // we're skipping first 128 bytes. its unplayable (header info)
+    
+    SNDFileFrames = snd_pcm_bytes_to_frames(SND2devP->pcm_handle,SNDFileSize);
+    
+    SFbuffer = (char*)malloc(SNDFileSize);
+    while ((SNDFileRead = fread(SFbuffer,4,SNDFileFrames,SNDFile)) > 0)
     {
-        fprintf("Header is RIFF\n");
-    } else {
-        fprintf("Header is Other?\n");
-    }
-}*/
-// -------------------------
-void HandleEvents()
+          snd_pcm_writei(SND2devP->pcm_handle,SFbuffer,SNDFileFrames);
+    }   
+    
+    return 1;
+}
+
+/* ======== STANDARD AS STUFF ============ */
+void HandleEvents(struct SND2dev * SND2devP)
 {   
     ASEvent event;
     Bool has_x_events = False ;
@@ -238,157 +215,82 @@ void HandleEvents()
         module_wait_pipes_input ( proc_message );
     }
 }
-
-void
-DispatchEvent (ASEvent * event)
+void DispatchEvent (ASEvent * event)
 {
-    //:show_activity("DIS_EVENT!: %i", event->x.type);
     SHOW_EVENT_TRACE(event);
-    switch (event->x.type)
+    switch (event->x.type)  
     {
         case PropertyNotify:
             handle_wmprop_event (Scr.wmprops, &(event->x));
             break;
-        default:
-            //show_activity("DIS_EVENT: %s", event->x.type);
-            break;
     }
-} 
-void proc_message (send_data_type type, send_data_type *body)
+}    
+void proc_message(send_data_type type, send_data_type *body)
 {
-        time_t        now = 0;
-        static time_t last_time = 0;
-        int code = -1 ;
-        //for debuggin
-        char timeBuff[128];
-        time_t curtime;
-        struct tm *loctime;
-        curtime = time(NULL);
-        loctime = localtime(&curtime);
-        strftime(timeBuff,128,"%H:%M:%S", loctime);
-        
-        // State Checking
-        int StateChange;
-        
-    if( type == M_PLAY_SOUND )
-    {
-        show_activity("M_PLAY_SND: proc_msg");
-               // CARD32 *pbuf = &(body[4]);
-        	//char *new_name = deserialize_string( &pbuf, NULL );
-               // SetupSoundEntry( EVENT_PlaySound, new_name);
-               // free( new_name );
-               // code = EVENT_PlaySound ;
-    }else if( (type&WINDOW_PACKET_MASK) != 0 )
-    {
-                // this bitwise appears to just return the type.. 
-//                show_activity("MASKING: %lX [MASK: %i]",type & WINDOW_PACKET_MASK,WINDOW_PACKET_MASK);
-                struct ASWindowData *wd = fetch_window_by_id( body[0] );
-                WindowPacketResult res ;
-                /* saving relevant client info since handle_window_packet could destroy the actuall structure */
-                ASFlagType old_state = wd?wd->state_flags:0;
-                res = handle_window_packet( type, body, &wd );
-                
-              // This Statement works (focus_change)..  
-//              if (type == M_FOCUS_CHANGE)
-//              {  
-//                  show_activity("FOCUS_CHANGE: 1"); 
-//              }
-              
-              if( res == WP_DataCreated )
-              {    
-                    // code = 0 :: Window/Menu created
-                    // code = 2 :: Window/Menu closed (exited)   
-                    code = EVENT_WindowAdded ;
-                    
-                   // show_activity("WindowADD: [%i]\n",code);
-              }else if( res == WP_DataChanged ) 
-              {
-                    if (type == M_FOCUS_CHANGE)
-                    { show_activity("FOC_CHANGE: 2 [%lX]:[%lX]", wd->state_flags, old_state); }
-                    
-                    
-                    StateChange = wd->state_flags ^ old_state;
-                    if (StateChange)
-                    {
-                        // State has Changed!
-                        // to see if its on/off: state_flags & [MASK]
-                        if (StateChange & AS_Shaded)
-                        {
-                            show_activity("STATE: Shade");
-                        }
-                        else if (StateChange & AS_Iconic)
-                        {
-                            show_activity("STATE: Iconic");
-                        }
-                        else if (StateChange & AS_Sticky)
-                        {
-                            show_activity("STATE: Sticky");
-                        }
-                        else if (StateChange & AS_Layer)
-                        {  show_activity("STATE: Layer"); }
-                        else if (StateChange & AS_Desktop)
-                        {  show_activity("STATE: Desktop"); }
-                        else if (StateChange & AS_Mapped)
-                        {
-                            // Same time as Iconic, Menu [open] (??)
-                            show_activity("STATE: Mapped");
-                        }
-                        else if (StateChange & AS_IconMapped)
-                        {
-                            show_activity("STATE: IconMapped");
-                        }
-                        else {
-                            show_activity("%s OTHER: [%lX]:[%lX]", timeBuff, wd->state_flags, old_state);
-                        }
-                        //----
-                        // AS_Withdrawn
-                        // AS_Dead
-                        //----
-                        // AS_MaximizedX
-                        // AS_MaximizedY
-                        
-                    }
-                 //   else {
-                 //        show_activity("%s DATA Changed! [%lX]",timeBuff, wd->state_flags);
-                 //        show_activity("%s DATA Change O:[%lX]",timeBuff, old_state);
-                 //        show_activity("%s DATA XOR: %lX",timeBuff, wd->state_flags ^ old_state);
-                 //   }
-                    
-                    
-
-                    res = -1;
-              }else if( res == WP_DataDeleted )
-              {
-                    code = EVENT_WindowDestroyed ;
-                    //show_activity("WindowDEST: [%i]\n",code);
-              }
-    }
-    // Works.... (viewport change)
-    //:else if( type == M_NEW_DESKVIEWPORT )
-    //:{
-    //:          code = EVENT_DeskViewportChanged ;
-    //:          // This should be returing "14" for code
-    //:          show_activity("ViewPort CHANGE: [%i]\n",code);
-    //:}
+    time_t now = 0;
+    static time_t last_time = 0;
+    int StateChange, code = -1;
     
-    now = time (0);
-    if( code >= 0 )
+    struct SND2dev SND2devS, *SND2devP;
+    SND2devP = &SND2devS;
+            
+
+
+    if (type == M_PLAY_SOUND) { show_activity("M_PLY_SND"); } // Need to find whats calling this type
+
+    else if ((type & WINDOW_PACKET_MASK) != 0)
     {
-           if ( now >= (last_time + (time_t) Config->delay))
-           {
-                if( ply_sound( code ) ) { last_time = now; }
-           }
+        struct ASWindowData *wd = fetch_window_by_id(body[0]);
+        WindowPacketResult res;
+        
+        ASFlagType old_state = wd ? wd->state_flags:0;
+        res = handle_window_packet(type,body,&wd);
+        
+        if (res == WP_DataCreated)
+        {
+            code = EVENT_WindowAdded;
+        }
+        else if (res == WP_DataChanged)
+        {
+            if (type == M_FOCUS_CHANGE)
+            {	show_activity("FOC_CHANGE"); }
+            
+            StateChange = wd->state_flags ^ old_state;
+            if (StateChange)
+            {
+                if (StateChange & AS_Shaded)
+                {
+                    show_activity("STATE: Shade");
+                    as_snd2_playsound(SND2devP);
+                }
+                if (StateChange & AS_Iconic)
+                {	show_activity("STATE: Iconic"); }
+                if (StateChange & AS_Sticky)
+                {	show_activity("STATE: Sticky"); }
+            }
+            res = -1;
+        }
+        else if (res == WP_DataDeleted)
+        {	code = EVENT_WindowDestroyed; }
+        
+    }
+    
+    now = time(0);
+    if (code >= 0)
+    {
+        if (now >= (last_time + (time_t)Config->delay))
+        {
+            if (ply_sound(code)) { last_time = now; }
+        }
     }
 }
+
 int ply_sound(int code)
 {
-    show_activity("Play Sound! %i\n",code);
+    // This function prolly needs removal
+    // (replaced with as_snd2_playsound()
+    show_activity("ply_sound %i\n",code);
     
     return 1;
-}
-Bool SetupSoundEntry(int code, char *sound)
-{
-    show_activity("SETsndENT: code [%i] SND [%c]\n",code,*sound);
-    return True;
 }
 
