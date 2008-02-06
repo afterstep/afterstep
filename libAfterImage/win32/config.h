e42b7c5096a34fe9a3de9c9aa7868c6040d719e5
sasha <sasha>
2008-02-06 21:31:17 +0000

    fixed png.h to properly include zlib.h on windows and undeffed stdint.h on win32

--- libAfterImage/win32/config.h
+++ libAfterImage/win32/config.h
@@ -77,7 +77,7 @@ typedef unsigned char boolean;
 #define HAVE_STDDEF_H 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#define HAVE_STDINT_H 1
+#undef HAVE_STDINT_H  /* VC++ does not have that ! */
 
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
