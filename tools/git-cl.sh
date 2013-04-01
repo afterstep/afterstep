861b6bf80f0f0b5c99468a9fea717a0a09f82e05
Sasha Vasko <sasha@aftercode.net>
2013-04-01 15:15:48 -0500

    updated configure script to remove old privacy notice and add install instructions.

--- tools/git-cl.sh
+++ tools/git-cl.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Generates changelog day by day
-echo "CHANGELOG" > ChangeLog
-echo ---------------------- >> ChangeLog
-git log --format="%cd %cn" --date=short | sort -u -r | while read DATE DUDE ; do
-    echo >> ChangeLog
-    echo [$DATE: $DUDE] >> ChangeLog
-    GIT_PAGER=cat git log --no-merges --format="%w(80,1,3)* %s" --committer="$DUDE" --since="$DATE 00:00:00" --until="$DATE 24:00:00" | grep -v "empty log message">> ChangeLog
+OUT_FILE=$1/ChangeLog
+echo "CHANGELOG" > $OUT_FILE
+echo ---------------------- >> $OUT_FILE
+git log --relative --format="%cd %cn" --date=short $1 | sort -u -r | while read DATE DUDE ; do
+    echo >> $OUT_FILE
+    echo [$DATE: $DUDE] >> $OUT_FILE
+    GIT_PAGER=cat git log --relative --no-merges --format="%w(80,1,3)* %s" --committer="$DUDE" --since="$DATE 00:00:00" --until="$DATE 24:00:00" $1 | grep -v "empty log message">> $OUT_FILE
 done
